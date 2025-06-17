beda363a6d328d37c0860adab81da17ba216a217
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2025-06-17 15:35:12 +0100

    learning sst, deployed on aws but no resources

---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+export default $config({
+  app(input) {
+    return {
+      name: "sst-nextjs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+      providers: {
+        aws: {
+          region: "us-east-1",
+        },
+      },
+    };
+  },
+  async run() {
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      access: "public",
+    });
+    new sst.aws.Nextjs("MyWeb", {
+      link: [bucket],
+    });
+  },
+});
